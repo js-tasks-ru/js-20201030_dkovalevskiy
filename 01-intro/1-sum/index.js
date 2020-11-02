1a5cad1a6ea0b3e3c9defd6b1e75b1115c6bfb95
Dkovalevskiy <dkovalevskiy@esetnod32.ru>
2020-11-02 14:47:00 +0300

    Добавлена функция для суммирования двух чисел.

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,5 +5,12 @@
  * @returns {number}
  */
 export default function sum(m, n) {
+  let checkStatusM = !m ? true : isNaN(Number(m));
+  let checkStatusN = !n ? true : isNaN(Number(n));
 
+  if (checkStatusM || checkStatusN) {
+    console.log('Введите два числа для получения суммы их сложения.');
+    return false;
+  }
+  return Number(m) + Number(n);
 }
